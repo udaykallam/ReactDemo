d487451ebb66dec513eb701d41da232451b58056
Uday <udayreddykallam@gmail.com>
2025-04-18 17:44:01 +0530

    Login page updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,14 @@ const Login = () => {
     <center>
         <h2>Login Page</h2>
         <br></br>
+        <label>Username </label>
+        <input type='text' name="uname" />
+        <br/><br/>
+        <label>Password </label>
+        <input type='text' name="pwd" />
+        <br/><br/>
+        <input type='submit' name="btn"/>
+        <br/><br/>
         <a href='/'>Back to Home</a>
     </center>
     </>
